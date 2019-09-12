68f42c507b88341391546bd599c5b1d6bf359771
Zein Miftah <zeinmiftah@gmail.com>
2019-09-12 11:37:16 +0700

    Update web/index.php

--- backend/web/index.php
+++ backend/web/index.php
@@ -5,15 +5,17 @@ error_reporting(E_ALL && ~E_NOTICE);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '../../../env.php';
-require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../../common/config/bootstrap.php';
-require __DIR__ . '/../config/bootstrap.php';
+$rootPath = dirname(dirname(dirname(__FILE__)));
+
+require $rootPath . '/vendor/autoload.php';
+require $rootPath . '/env.php';
+require $rootPath . '/vendor/yiisoft/yii2/Yii.php';
+require $rootPath . '/common/config/bootstrap.php';
+require $rootPath . '/backend/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../config/main.php'
+    require $rootPath . '/common/config/main.php',
+    require $rootPath . '/backend/config/main.php'
 );
 
 (new yii\web\Application($config))->run();
